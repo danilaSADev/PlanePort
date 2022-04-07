991f09bd4f219b06fa79efa8ab0ed4cd9bb2f89d
danyil.korovin <danilas25up@gmail.com>
2022-04-07 23:16:50 +0300

    initial commit

---
+++ ClientApp/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render () {
+    return (
+      <div>
+        <h1>Starting React.JS!</h1>
+        <p>Welcome to your new single-page application, built with: <b>REACT.JS</b></p>
+        <h1><strong>FUCK THE UNIVERSITY!!!</strong></h1>
+      </div>
+    );
+  }
+}
