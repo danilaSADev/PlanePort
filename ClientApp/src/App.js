42bd0a4943619ca0ff2493f1e19055bd027b1985
danyil.korovin <danilas25up@gmail.com>
2022-04-09 23:35:21 +0300

    improved hero page

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,27 +2,21 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
 import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
 import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
 
 import './custom.css'
-import Welcome from './components/Welocme';
 
 export default class App extends Component {
   static displayName = App.name;
 
   render () {
     return (
-      <Welcome></Welcome>
-      // <Layout>
-      //   <Route exact path='/' component={Home} />
-      //   <Route path='/counter' component={Counter} />
-      //   <AuthorizeRoute path='/fetch-data' component={FetchData} />
-      //   <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
-      // </Layout>
+        <Layout>
+            <Route exact path='/' component={Home} />
+            <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
+        </Layout>
     );
   }
 }
--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Col, Container, Row } from 'reactstrap';
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -6,8 +7,43 @@ export class Home extends Component {
   render () {
     return (
       <div>
-        <h1>Welcome to our planes website!</h1>
-        <p> This is single page application written in : <b>REACT.JS</b></p>
+        <Container>
+            <Row style={{
+                backgroundImage: 'url("/assets/images/plane_1.jpg")',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '700px' 
+            }}>
+                <Col className="text-light text-center" style={{
+                    marginTop: 'auto',
+                    marginBottom: 'auto',
+                    backgroundColor: 'rgba(0,0,0,0.5)'
+                }}>
+                    <hr/>
+                    <h1>PlanePort</h1>
+                    <h5>Book tickets with no worries!</h5>
+                    <hr/>
+                </Col>
+            </Row>
+            <hr/>
+            <Row>
+                <Col>
+                    <img src='/assets/images/airport_image_1.jpg'/>
+                </Col>
+                <Col className='text-justify' style={{
+                    marginTop: 'auto',
+                    marginBottom: 'auto'
+                }}>
+                    <h3 className='text-center'>. . .</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                    <h3 className='text-center'>. . .</h3>
+                </Col>
+            </Row>
+            <br/>
+        </Container>
       </div>
     );
   }
